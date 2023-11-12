08c40e058cf39d94dc8a20567ac0fa3592386fc4
zhangmingyuan <2369558390@qq.com>
2023-11-12 16:35:18 +0800

    分离main函数

--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,25 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let file_path = &args[2];
+    let config = parse_config(&args);
 
-    println!("searching for query: {}", query);
-    println!("filePath: {}", file_path);
+    println!("searching for query: {}", config.query);
+    println!("filePath: {}", config.file_path);
 
-    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let contents =
+        fs::read_to_string(config.file_path).expect("Should have been able to read the file");
 
     println!("With text:\n{contents}");
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let file_path = args[2].clone();
+
+    Config { query, file_path }
 }
