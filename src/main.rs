08d38e3d394bfe5fa11d2769285e89c4ec3f497f
zhangmingyuan <2369558390@qq.com>
2023-11-12 17:00:11 +0800

    增加panic

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use std::fs;
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let config = parse_config(&args);
+    let config: Config = Config::new(&args);
 
     println!("searching for query: {}", config.query);
     println!("filePath: {}", config.file_path);
@@ -21,9 +21,17 @@ struct Config {
     file_path: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
+impl Config {
+    fn new(args: &[String]) -> Config {
+        if args.len() < 3 {
+            panic!("not enough arguments");
+        }
 
-    Config { query, file_path }
+        let query: String = args[1].clone();
+        let file_path: String = args[2].clone();
+
+        Config { query, file_path }
+    }
 }
+
+// cargo run -- the poem.txt
